04e3b085fae5a55f83f8422e6f5ed10c45ee5e45
Daniele Giardini <daniele.giardini@gmail.com>
2015-04-21 18:28:17 +0200

    Fixed DOTween.TweensBytId not working correctly in some cases

--- _DOTween.Assembly/DOTween/DOTween.cs
+++ _DOTween.Assembly/DOTween/DOTween.cs
@@ -32,7 +32,7 @@ namespace DG.Tweening
     public class DOTween
     {
         /// <summary>DOTween's version</summary>
-        public static readonly string Version = "1.0.600";
+        public static readonly string Version = "1.0.605";
 
         ///////////////////////////////////////////////
         // Options ////////////////////////////////////
