7545637ccac3d544316b36456ed24d144f426574
Daniele Giardini <daniele.giardini@gmail.com>
2015-05-29 11:16:30 +0200

    Content update

--- UnityTests.Unity5/Assets/_Tests/TempTests.cs
+++ UnityTests.Unity5/Assets/_Tests/TempTests.cs
@@ -1,23 +1,46 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using DG.Tweening;
-using System;
+using DG.Tweening.Core;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class TempTests : BrainBase
 {
+	public Transform targetA, targetB;
+
+	public DOGetter<Vector3> getterA, getterB;
+
 	IEnumerator Start()
 	{
-		yield return new WaitForSeconds(0.8f);
+		yield return new WaitForSeconds(1);
 
-		Debug.Log(Time.realtimeSinceStartup + " Create tween");
+		getterA = ()=> targetA.position;
+		getterB = ()=> targetB.position;
 
-		DOTween.Sequence()
-            .SetId(123)
-            .PrependInterval(3)
-            .OnComplete(() => Debug.Log(Time.realtimeSinceStartup + " First callback!"));
+		Debug.Log(getterA == getterB);
+		Debug.Log(getterA.Equals(getterB));
 
-        DOTween.Kill(123, true);
+		float time = Time.realtimeSinceStartup;
+		Debug.Log(CompareGetters(getterA, getterB));
+		float elapsed = Time.realtimeSinceStartup - time;
+		Debug.Log("Compare executed in " + elapsed + " seconds");
+	}
+
+	bool CompareGetters<T>(DOGetter<T> a, DOGetter<T> b)
+	{
+		var aBody = a.Method.GetMethodBody().GetILAsByteArray();
+		var bBody = b.Method.GetMethodBody().GetILAsByteArray();
+
+		int aLen = aBody.Length;
+		Debug.Log("LEN: " + aLen);
+		if (aLen != bBody.Length) return false;
+
+		for(int i = 0; i < aLen; i++) {
+			if(aBody[i] != bBody[i]) return false;
+		}
+		return true;
 	}
 }
