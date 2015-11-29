19e2b30586d3bb2149a418be2ae688fbdaa2beac
Demigiant <daniele.giardini@gmail.com>
2015-11-29 20:31:47 +0100

    Added screenshots

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # dotween
 
-A Unity C# animation engine. HOTween v2
-
+A Unity C# animation engine. HOTween v2  
 Developed by Daniele Giardini - http://www.demigiant.com
 
 Check the docs on DOTween's website - http://dotween.demigiant.com
+
+![DOTween](screenshots/splash_dotween.png "DOTween")  
+![DOTween](screenshots/splash_lambda.png "DOTween")  
+![DOTween](screenshots/splash_shortcuts.png "DOTween")  
--- UnityTests.Unity5/Assets/_Tests/TempTests.cs
+++ UnityTests.Unity5/Assets/_Tests/TempTests.cs
@@ -13,56 +13,8 @@ public class TempTests : BrainBase
 
     void Start()
     {
-        DOTween.Init();
-        PlayTween();
-    }
-
-    Sequence mySequence;
-
-    Tween anchorMinTween = null;
-    Tween anchorMaxTween = null;
-    Tween anchorPositionTween = null;
-    Tween scaleTween = null;
-
-    void PlayTween()
-    {
-        mySequence = DOTween.Sequence();
-
-        anchorMaxTween = DOTween.To(() => rectTrans.anchorMax, v => rectTrans.anchorMax = v, new Vector2(0.5f, 0.5f), 1f)
-           // .Pause()
-           .SetDelay(2f)
-           .OnPlay(()=> Debug.Log(Time.realtimeSinceStartup + " Start MAX"))
-            ;
-
-        anchorMinTween = DOTween.To(() => rectTrans.anchorMin, v => rectTrans.anchorMin = v, new Vector2(0.5f, 0.5f), 1f)
-           // .Pause()
-           .SetDelay(2f)
-           .OnPlay(() => Debug.Log(Time.realtimeSinceStartup + " Start MIN"))
-            ;
-
-        anchorPositionTween = DOTween.To(() => rectTrans.anchoredPosition, v => rectTrans.anchoredPosition = v, Vector2.zero, 1f)
-            //.Pause()
-            .SetDelay(2f)
-            .OnPlay(() => Debug.Log(Time.realtimeSinceStartup + " Start POS"))
-            ;
-
-        scaleTween = DOTween.To(() => rectTrans.localScale, v => rectTrans.localScale = v, Vector3.zero, 1f)
-            //.Pause()
-            .SetDelay(2f)
-            .OnPlay(() => Debug.Log(Time.realtimeSinceStartup + " Start SCALE"))
-            ;
-
-        mySequence
-            .AppendInterval(5f)
-            .Append(anchorMaxTween)
-            .Join(anchorMinTween)
-            .Join(anchorPositionTween)
-            .AppendInterval(2f)
-            .Append(scaleTween)
-            //.SetDelay(2f)
-            //.Play()
-            .Pause()
-            .OnPlay(()=> Debug.Log(Time.realtimeSinceStartup + " >>> PLAY"))
-            ;
+    	DOTween.Sequence().SetDelay(2)
+    		// .AppendInterval(2)
+    		.Append(rectTrans.DOAnchorPosX(100, 1).SetRelative());
     }
 }
