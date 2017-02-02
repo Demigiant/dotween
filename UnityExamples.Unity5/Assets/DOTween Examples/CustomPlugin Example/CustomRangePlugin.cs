07dff0075f67bd87fb5494c095e63002a40062be
Demigiant <daniele.giardini@gmail.com>
2017-02-02 11:37:00 +0100

    Updated examples project

--- UnityExamples.Unity5/Assets/DOTween Examples/CustomPlugin Example/CustomRangePlugin.cs
+++ UnityExamples.Unity5/Assets/DOTween Examples/CustomPlugin Example/CustomRangePlugin.cs
@@ -53,7 +53,6 @@ public class CustomRangePlugin : ABSTweenPlugin<CustomRange, CustomRange, NoOpti
     // Calculates the value based on the given time and ease
     public override void EvaluateAndApply(NoOptions options, Tween t, bool isRelative, DOGetter<CustomRange> getter, DOSetter<CustomRange> setter, float elapsed, CustomRange startValue, CustomRange changeValue, float duration, bool usingInversePosition, UpdateNotice updateNotice)
     {
-        CustomRange res = getter();
         float easeVal = EaseManager.Evaluate(t, elapsed, duration, t.easeOvershootOrAmplitude, t.easePeriod);
 
         // Here I use startValue directly because CustomRange a struct, so it won't reference the original.
