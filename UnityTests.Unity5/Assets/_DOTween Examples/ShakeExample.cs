2b81429fec691da6c8bb5f1d6189de7d2a479366
Demigiant <daniele.giardini@gmail.com>
2017-06-02 02:31:46 +0200

    - minor

---
+++ UnityTests.Unity5/Assets/_DOTween Examples/ShakeExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ShakeExample : MonoBehaviour
+{
+    public Transform target;
+    public Tween shakeTween;
+
+    void Start()
+    {
+        // Create the tween (using default values)
+        shakeTween = target.DOShakePosition(1.5f).SetAutoKill(false).Pause();
+    }
+
+    // Called by UI button
+    public void Shake()
+    {
+        shakeTween.Restart();
+    }
+}
--- UnityTests.Unity5/Assets/_Tests/TempTests.cs
+++ UnityTests.Unity5/Assets/_Tests/TempTests.cs
@@ -10,31 +10,9 @@ using UnityEngine;
 public class TempTests : BrainBase
 {
     public Transform target;
-    Tween myTween;
-    bool aBool;
 
     void Start()
     {
-    	DOTween.Init();
-    	this.StartCoroutine(COUpdate());
-    }
-
-    IEnumerator COUpdate()
-    {
-    	yield return null;
-    	yield return null;
-    	target.DOMoveX(2, 7);
-    	yield return null;
-    	// for (int i = 0; i < 1000; ++i) target.DOMoveX(2, 7).OnComplete(ACallback);
-    	// for (int i = 0; i < 1000; ++i) target.DOMoveX(2, 7).OnComplete(()=> aBool = true);
-    	target.DOMoveX(2, 7).OnComplete(()=> aBool = true);
-    	// myTween.Kill();
-    	// myTween = target.DOMoveX(2, 1);
-    	// myTween.OnComplete(ACallback);
-    }
-
-    void ACallback()
-    {
-    	Debug.Log("DO SOMETHING");
+        target.DOShakeScale(2, 0.15f, 10, 90f, true);
     }
 }
