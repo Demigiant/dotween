cbc30f3aa1f49140c493567b9864f2e0ba92fa0f
Demigiant <daniele.giardini@gmail.com>
2018-07-17 12:05:57 +0200

    - minor

--- _DOTween.Assembly/DOTween/DOTween.cs
+++ _DOTween.Assembly/DOTween/DOTween.cs
@@ -32,7 +32,7 @@ namespace DG.Tweening
     public class DOTween
     {
         /// <summary>DOTween's version</summary>
-        public static readonly string Version = "1.2.002"; // Last version before modules: 1.1.755
+        public static readonly string Version = "1.2.003"; // Last version before modules: 1.1.755
 
         ///////////////////////////////////////////////
         // Options ////////////////////////////////////
