90885a190607bb4d1bbde081165ab26005390d32
Demigiant <daniele.giardini@gmail.com>
2019-03-01 20:55:11 +0100

    [DeExtensions] Added Vector3.RotateAroundPivot

--- _DOTween.Assembly/DOTween/Core/Utils.cs
+++ _DOTween.Assembly/DOTween/Core/Utils.cs
@@ -40,6 +40,11 @@ namespace DG.Tweening.Core
             return ang;
         }
 
+        internal static Vector3 RotateAroundPivot(Vector3 point, Vector3 pivot, Quaternion rotation)
+        {
+            return rotation * (point - pivot) + pivot;
+        }
+
         /// <summary>
         /// Uses approximate equality on each axis instead of Unity's Vector3 equality,
         /// because the latter fails (in some cases) when assigning a Vector3 to a transform.position and then checking it.
@@ -52,7 +57,7 @@ namespace DG.Tweening.Core
         }
 
         /// <summary>
-        /// Looks for the type withing all possible project assembly names
+        /// Looks for the type within all possible project assembly names
         /// </summary>
         internal static Type GetLooseScriptType(string typeName)
         {
