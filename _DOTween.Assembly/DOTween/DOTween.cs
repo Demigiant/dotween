75120f0903bae224e63b77b81ed6e1d022011a6d
Daniele Giardini <daniele.giardini@gmail.com>
2026-02-06 11:00:53 +0100

    polish

--- _DOTween.Assembly/DOTween/DOTween.cs
+++ _DOTween.Assembly/DOTween/DOTween.cs
@@ -35,7 +35,7 @@ namespace DG.Tweening
     public class DOTween
     {
         /// <summary>DOTween's version</summary>
-        public static readonly string Version = "1.2.815"; // Last version before modules: 1.1.755, last version before defines: 1.2.790
+        public static readonly string Version = "1.2.815"; // Last version before modules: 1.1.755, last version before switch to defines: 1.2.790
 #if DEBUG
         internal const string DebugPrefix = "<color=#ff0000><b>//</b></color> "; // Used by logs that are only thrown in debug version of DOTween
 #endif
